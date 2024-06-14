abe121d66f5cc9878a92c83ee8521461d57bea90
Spencer Dunn <98668127+SpencerD01@users.noreply.github.com>
2024-06-14 15:00:08 -0400

    Added seed for 25ff71a9
    Added seed for 25ff71a9, including main and generate_input functions.

---
+++ seeds/25ff71a9.py
@@ -0,0 +1,51 @@
+from common import *
+
+import numpy as np
+from typing import *
+
+
+# concepts:
+# sliding objects
+
+# description:
+# In the input you will see a 3x3 grid with a contiguous shape on it.
+# Slide the shape down by one pixel.
+
+def main(input_grid):
+    # find the connected component, which is a monochromatic object
+    object = find_connected_components(input_grid, background=Color.BLACK, connectivity=8, monochromatic=True)[0]
+
+    # get output_grid ready
+    output_grid = np.zeros((3, 3))
+
+    # slide the object in the output grid by one index down
+    blit(output_grid, object, y=1)
+
+    return output_grid
+
+
+def generate_input():
+    # first create blank grid
+    grid = np.zeros((3, 3), dtype=int)
+
+    # while the grid is empty
+    while grid.sum() == 0:
+
+        # generate a random up to 3x3 sprite
+        n = random.randint(1, 3)
+        m = random.randint(1, 2)
+        random_sprite_to_add = random_sprite(n, m, symmetry='not_symmetric')
+
+        # choose a random location on the grid, ensuring the bottom row is empty
+        x = random.randint(0, 2)
+        y = random.randint(0, 2 - m)
+
+        # place the triangle onto the grid
+        blit(grid, random_sprite_to_add, x, y)
+
+    return grid
+
+# ============= remove below this point for prompting =============
+
+if __name__ == '__main__':
+    visualize(generate_input, main)
