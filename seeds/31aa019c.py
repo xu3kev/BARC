3d588c8b6e7b2ab11c3ae7cb94bf67f03062dad1
ellisk42@gmail.com <ellisk42@gmail.com>
2024-06-15 08:30:05 -0400

    improved 31aa019c

--- seeds/31aa019c.py
+++ seeds/31aa019c.py
@@ -7,40 +7,38 @@ from typing import *
 # counting, uniqueness, surrounding
 
 # description:
-# In the input, you will see a grid with 10x10 grid with many colored pixels.
-# To make the output, find the cell that is uniquely colored, that is that cell is the only one of that color, then surround that cell with red blocks. Make all the other cells black.
+# In the input, you will see a grid with a black background and colored pixels sprinkled on it. Exactly one color occurs only one time.
+# To make the output, find the cell whose color is unique (color occurs only one time), and surround that cell with red pixels. Make all the other pixels black.
 
 def main(input_grid: np.ndarray) -> np.ndarray:
-   #find the index and value of the unique cell
-    row=np.size(input_grid,axis=0)
-    col = np.size(input_grid,1)
-    frequency_dict = {}
-    unique = 0
-    for i in range(row):
-        for j in range(col):
-            key = input_grid[i][j]
-            if key in frequency_dict:
-                frequency_dict[key] +=1
-            else:
-                frequency_dict[key] = 1
-    #find the value of the unique cell
-    for key in frequency_dict:
-        freq = frequency_dict[key]
-        if freq == 1:
-            unique = key
-    #find index
-    result = np.where(input_grid == unique)
-    row_index, col_index = result[0][0], result[1][0]
+    # Plan:
+    # 1. Create a blank new canvas (so that the non-unique colors don't get copied)
+    # 2. Find the unique cell
+    # 3. Surround the unique cell with red pixels
+
+    output_grid = np.zeros_like(input_grid)
+
+    # 2. Find the unique cell
+    unique_color = None
+    for color in Color.NOT_BLACK:
+        if np.count_nonzero(input_grid == color) == 1:
+            unique_color = color
+            break
     
-    #create output grid
-    output_grid = np.zeros((row,col), dtype=int)
-    #set the neighbors to the RED color
-    for i in range(row_index - 1, row_index + 2):
-        for j in range(col_index - 1, col_index + 2):
-            output_grid[i][j]=Color.RED
-    #set the cell to be the unique color
-    output_grid[row_index][col_index] = unique
+    # 3. Surround the unique cell with red pixels
+    # First get the coordinates of the unique cell
+    x, y, width, height = bounding_box(input_grid == unique_color)
+    # Copy red over the region around the unique cell (but this will accidentally delete the unique cell, so be copied back)
+    for i in range(x-1, x+2):
+        for j in range(y-1, y+2):
+            if 0 <= i < len(input_grid) and 0 <= j < len(input_grid[0]):
+                output_grid[i, j] = Color.RED
+    # Copy the unique cell back
+    output_grid[x, y] = unique_color
+
     return output_grid
+
+
     
 
 
@@ -49,41 +47,30 @@ def generate_input() -> np.ndarray:
     
     # make a 10x10 black grid first as background
     n, m = 10, 10
-    matrix = np.zeros((n, m), dtype=int)
+    grid = np.zeros((n, m), dtype=int)
 
-    #randomly select a unique color from Color.NOT_BLACK
+    # randomly select a unique color from Color.NOT_BLACK
     unique_color = random.choice(Color.NOT_BLACK)
 
-    #randomly choose a non-border cell for the unique color
-    non_border_cells = [(i, j) for i in range(1, 9) for j in range(1, 9)]
+    # randomly choose a non-border cell for the unique color
+    non_border_cells = [ (i, j) for i in range(1, n-1) for j in range(1, m-1) ]
     unique_cell = random.choice(non_border_cells)
-    matrix[unique_cell] = unique_color
+    grid[unique_cell] = unique_color
 
-    #remove the unique color from the list
-    remaining_colors = [color for color in Color.NOT_BLACK if color != unique_color]
+    # remove the unique color from the list
+    remaining_colors = [ color for color in Color.NOT_BLACK if color != unique_color ]
 
-    #generate frequencies for the remaining non-unique colors
-    frequencies = []
-    total_cells = 0
-    #ensures that there are between 20 and 40 cells colored
-    while total_cells < 20 or total_cells > 40:
-        frequencies = [random.choice([0, 2, 3, 4, 5, 6]) for _ in range(8)]
-        total_cells = sum(frequencies)
-        
-    #assign the remaining colors to the matrix
-    color_cells = []
-    for color, freq in zip(remaining_colors, frequencies):
-        color_cells.extend([color] * freq)
+    for remaining_color in remaining_colors:
+        # Pick a random frequency but make sure that this colour is not unique (does not have frequency 1)
+        frequency_of_this_color = random.choice([0, 2, 3, 4, 5, 6])
 
-    random.shuffle(color_cells)
-
-    #place colors in the matrix, avoiding the unique cell
-    available_cells = [(i, j) for i in range(10) for j in range(10) if (i, j) != unique_cell]
-    random.shuffle(available_cells)
-    #randomly assigning the avialble cells to their color
-    for cell, color in zip(available_cells, color_cells):
-        matrix[cell] = color
-    return matrix
+        for _ in range(frequency_of_this_color):
+            # randomly choose an unoccupied cell for the remaining color
+            empty_cells = [ (i, j) for i in range(n) for j in range(m) if grid[i][j] == Color.BLACK ]
+            x, y = random.choice(empty_cells)
+            grid[x, y] = remaining_color
+    
+    return grid
   
 
 
