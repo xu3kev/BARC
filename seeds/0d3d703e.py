4a85ebcb4fde5e899068ddac959bd2f7aa860693
ellisk42@gmail.com <ellisk42@gmail.com>
2024-06-19 04:58:44 -0400

    improved 017c7

--- seeds/0d3d703e.py
+++ seeds/0d3d703e.py
@@ -7,16 +7,16 @@ from typing import *
 # color mapping
 
 # description:
-# The input is a 3x3 grid where each column is of the same color. 
-# You will output the corresponding color mapping in a 3x3 grid with each column being the same color. 
+# The input is a grid where each column is of the same color. 
+# To make the output, change each color according to the following mapping:
+# green -> yellow, blue -> gray, red -> pink, teal -> maroon, yellow -> green, gray -> blue, pink -> red, maroon -> teal
 
 def main(input_grid):
     # Initialize output grid
     output_grid = input_grid.copy()
 
-    # Performs color mapping by columns
-    for i in range(output_grid.shape[0]):
-      output_grid[i] = color_map[output_grid[i,0]]
+    # Performs color mapping
+    output_grid = np.vectorize(lambda color: color_map.get(color, color))(output_grid)
 
     return output_grid
     
@@ -33,26 +33,10 @@ color_map = {Color.GREEN : Color.YELLOW,
 
 
 def generate_input():
-  # Choosing 3 different colors to color the columns that is not orange or black
-  color1 = random.choice(list(Color.NOT_BLACK))
-  while color1 == Color.ORANGE:
-     color1 = random.choice(list(Color.NOT_BLACK))
-  color2 = random.choice(list(Color.NOT_BLACK))
-  # Check if color 1 and 2 are the same 
-  while color1 == color2 or color2 == Color.ORANGE:
-     color2 = random.choice(list(Color.NOT_BLACK))
-  color3 = random.choice(list(Color.NOT_BLACK))
-  # Check if color3 is same as color1 or color2
-  while color1==color3 or color2 == color3 or color3 == Color.ORANGE:
-     color3 = random.choice(list(Color.NOT_BLACK))
-
-  # Creating the grid 
-  grid = np.zeros((3,3), dtype=int)
-  grid[0] = color1 
-  grid[1] = color2
-  grid[2] = color3
-  
-  return grid
+    grid = np.full((3, 3), Color.BLACK)
+    for x in range(grid.shape[0]):
+        grid[x, :] = random.choice(list(color_map.keys()))
+    return grid
 # ============= remove below this point for prompting =============
 
 if __name__ == '__main__':
