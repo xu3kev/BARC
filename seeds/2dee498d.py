3bf35e1b0b340bac593c749da3c32aabfeb2f094
ellisk42@gmail.com <ellisk42@gmail.com>
2024-06-19 07:54:26 -0400

    2dee498d refactor

--- seeds/2dee498d.py
+++ seeds/2dee498d.py
@@ -4,43 +4,40 @@ import numpy as np
 from typing import *
 
 # concepts:
-# repetition, reflection
+# translational symmetry, symmetry detection, reflection
 
 # description:
-# In the input, you will receive some repetitions and reflections (the first occurence
-# is guaranteed not a reflection) of a sprite laid horizontally right next to each other. 
-# In the output, you want to identify the sprite. 
+# In the input, you will see a sprite repeated horizontally, and some of those repetitions might be reflected top/down/right/left.
+# To make the output, just extract the repeated sprite.
 
 def main(input_grid):
-    # Initialize output grid
-    sprite = input_grid.copy()
+    # Find the period, remembering that we need to consider reflections
+    for period in range(1, input_grid.shape[0]):
+        # Extract the sprite and all of its repeated translated versions
+        sprite = input_grid[:period]
+        repetitions = [ input_grid[i*period:(i+1)*period] for i in range(input_grid.shape[0]//period) ]
 
-    # Detecting patterns in the input grid
-    for num_pattern_cols in range(1,input_grid.shape[0]):
-        if input_grid.shape[0] % num_pattern_cols == 0:
-            # Check complete repeating patterns
-            check = True
-            for i in range(num_pattern_cols, input_grid.shape[0],num_pattern_cols):
-                if i+num_pattern_cols <= input_grid.shape[0] and not np.equal(input_grid[0:num_pattern_cols,:], input_grid[i:i+num_pattern_cols,:]).all() and not np.equal(input_grid[0:num_pattern_cols,:], np.flip(input_grid[i:i+num_pattern_cols,:],axis = 1)).all():
-                    check = False 
-                
-        if check:
-            # Recognizing pattern in input grid and changes its color
-            sprite = input_grid[0:num_pattern_cols]
-          
+        # Check that every repetition matches the sprite, or a reflection of the sprite
+        valid = True
+        for rep in repetitions:
+            reflections = [rep, np.flip(rep, 0), np.flip(rep, 1)]
+            if not any([np.array_equal(sprite, r) for r in reflections]):
+                valid = False
+
+        if valid:
             return sprite
         
-    return sprite
+    assert False, "No valid period found"
 
 def generate_input():
-    # Create the sprite to be duplicated
+    # Create the sprite to be duplicated; pick a trio of random colors
     n = random.randint(2,7)
-    sprite = random_sprite(n,n,color_palette=random.sample(list(Color.NOT_BLACK),3))
+    sprite = random_sprite(n, n, color_palette=random.sample(list(Color.NOT_BLACK),3))
     
     # Duplicate the sprite 3 times horizontally
-    grid = np.zeros((3*n,n),dtype=int)
+    grid = np.zeros((3*n, n),dtype=int)
     for i in range(3):
-        blit(grid, sprite, i*n,0)
+        blit(grid, sprite, x=i*n, y=0)
 
     return grid
 
