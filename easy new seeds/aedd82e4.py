ad956c8c9d8e880eb5848f74c7603ccbb891914c
Keya Hu <kh844@g2-login-05.coecis.cornell.edu>
2024-09-15 00:06:30 -0400

    three easy new seeds

---
+++ easy new seeds/aedd82e4.py
@@ -0,0 +1,63 @@
+from common import *
+
+import numpy as np
+from typing import *
+
+# concepts:
+# Isoalted object detection, color changing
+
+# description:
+# In the input you will see a grid with red pattern
+# To make the output grid, you should find out the single isolated red object with size of 1x1 and change it to blue.
+
+def main(input_grid):
+    # Detect all the red objects in the grid.
+    objects = detect_objects(grid=input_grid, colors=[Color.RED], monochromatic=True, connectivity=4)
+    output_grid = input_grid.copy()
+    for object in objects:
+        x, y, length, width = bounding_box(grid=object)
+        # Find out the single isolated red object with size of 1x1 and change it to blue.
+        if length == 1 and width == 1:
+            output_grid[x, y] = Color.BLUE
+
+    return output_grid
+
+def generate_input():
+    # Generate the background grid with size of n x m.
+    n, m = np.random.randint(3, 6), np.random.randint(3, 6)
+    grid = np.zeros((n, m), dtype=int)
+
+    # Randomly scatter color pixels on the grid.
+    def random_scatter_point_on_grid(grid, color, density):
+        n, m = grid.shape
+        colored = 0
+        # Randomly scatter density of color pixels on the grid.
+        while colored < density * n * m:
+            x = np.random.randint(0, n)
+            y = np.random.randint(0, m)
+            if grid[x, y] == Color.BLACK:
+                grid[x, y] = color
+                colored += 1
+        return grid
+    
+    # Randomly scatter color pixels on the grid.
+    grid = random_scatter_point_on_grid(grid, Color.RED, 0.5)
+
+    # Ensure there is at least one 1x1 single isolated red object in the grid.
+    random_x, random_y = np.random.randint(0, n), np.random.randint(0, m)
+    grid[random_x, random_y] = Color.RED
+    if random_x > 0:
+        grid[random_x - 1, random_y] = Color.BLACK 
+    if random_x < n - 1:
+        grid[random_x + 1, random_y] = Color.BLACK
+    if random_y > 0:
+        grid[random_x, random_y - 1] = Color.BLACK
+    if random_y < m - 1:
+        grid[random_x, random_y + 1] = Color.BLACK
+
+    return grid
+
+# ============= remove below this point for prompting =============
+
+if __name__ == '__main__':
+    visualize(generate_input, main)
---
+++ easy new seeds/e9614598.py
@@ -0,0 +1,69 @@
+from common import *
+
+import numpy as np
+from typing import *
+
+# concepts:
+# Isoalted object detection, pattern drawing
+
+# description:
+# In the input you will see two blue pixels on the grid with a distance of 5 pixels.
+# To make the output grid, you should place a 3x3 green cross pattern between the two blue pixels.
+
+def main(input_grid):
+    # Detect the two blue pixels on the grid.
+    blue_pixels = detect_objects(grid=input_grid, colors=[Color.BLUE], monochromatic=True, connectivity=4)
+
+    # Get the position of the two blue pixels.
+    x_two, y_two = [], []
+    for blue_pixel in blue_pixels:
+        x, y, _, _ = bounding_box(grid=blue_pixel)
+        x_two.append(x)
+        y_two.append(y)
+    
+    # Find out if the two blue pixels are horizontal or vertical.
+    if_horizontal = y_two[0] == y_two[1]
+
+    # Generate the 3x3 green cross pattern between the two blue pixels.
+    object_pattern = np.array( [[Color.BLACK, Color.GREEN, Color.BLACK],
+                                [Color.GREEN, Color.GREEN, Color.GREEN],
+                                [Color.BLACK, Color.GREEN, Color.BLACK]])
+    output_grid = input_grid.copy()
+
+    # Place the 3x3 green cross pattern between the middle of two blue pixels.
+    if if_horizontal:
+        output_grid = blit_sprite(grid=output_grid, sprite=object_pattern, x=x_two[0] + 2, y=y_two[0] - 1)
+    else:
+        output_grid = blit_sprite(grid=output_grid, sprite=object_pattern, x=x_two[0] - 1, y=y_two[0] + 2)
+    return output_grid
+
+def generate_input():
+    # Generate the background grid with size of n x m.
+    n, m = 10, 10
+    grid = np.zeros((n, m), dtype=int)
+
+    # Randomly choose if the two blue pixels are horizontal or vertical.
+    if_horizontal  = np.random.choice([True, False])
+
+    # Randomly place two blue pixels on the grid with a distance of 5 pixels.
+    line_width = 5
+
+    # Ensure there is enough space for the 3 x 3 green cross pattern and two blue pixels.
+    if if_horizontal:
+        x = np.random.randint(0, n - 6)
+        y = np.random.randint(1, m - 1)
+        grid[x, y] = Color.BLUE
+        grid[x + line_width + 1, y] = Color.BLUE
+    else:
+        x = np.random.randint(1, n - 1)
+        y = np.random.randint(0, m - 6)
+        grid[x, y] = Color.BLUE
+        grid[x, y + line_width + 1] = Color.BLUE
+
+    return grid
+
+
+# ============= remove below this point for prompting =============
+
+if __name__ == '__main__':
+    visualize(generate_input, main)
