e9927663fa8eb247183f10543e562625fe3ff26b
ellisk42@gmail.com <ellisk42@gmail.com>
2024-10-09 07:16:52 -0400

    conceptarc

--- view_problem.py
+++ view_problem.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from arc import train_problems, validation_problems
+from conceptarc import concept_arc_problems
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import numpy as np
@@ -75,7 +76,7 @@ if __name__ == '__main__':
 
     problem_id = sys.argv[1]
 
-    problem = [p for p in train_problems+validation_problems if p.uid == problem_id]
+    problem = [p for p in train_problems+validation_problems+concept_arc_problems() if p.uid == problem_id]
     assert len(problem) == 1, f"Problem {problem_id} not found"
 
     n_pairs = len(problem[0].train_pairs) + len(problem[0].test_pairs)
