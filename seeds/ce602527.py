bb8f486b6a645650d3ff456b4d29fa896b8a8884
ellisk42@gmail.com <ellisk42@gmail.com>
2024-10-16 06:25:22 -0400

    simplified seeds/ce602527.py

--- seeds/ce602527.py
+++ seeds/ce602527.py
@@ -4,100 +4,104 @@ import numpy as np
 from typing import *
 
 # concepts:
-# scaling, pattern matching
+# scaling, shape matching, non-black background
 
 # description:
-# In the input you should see three objects, two of which are the same pattern but different sizes and colors. The third object is a different pattern.
-# To make the output, you need to find the two objects that are the same pattern but different sizes and colors.
-# Return the smaller object in the same pattern.
+# In the input you should see three objects, two of which are the same shape but different sizes and colors. The third object is a different shape.
+# To make the output, you need to find the two objects that are the same shape but different sizes and colors.
+# Return the smaller object in the same shape.
 
 def main(input_grid):
+    # Plan:
+    # 1. Find the background color
+    # 2. Extract objects by color
+    # 3. Define a helper function to check if two objects are the same shape but different color/scale, remembering that the bigger one might be partially out of the canvas
+    # 4. Iterate all candidate objects and check if they are the same shape but different color/scale
+    # 5. Return what we find
+
     # Determine the background color, which is the most common color in the grid
     colors = np.unique(input_grid)
     background = colors[np.argmax([np.sum(input_grid == c) for c in colors])]
     object_colors = [c for c in colors if c != background]
 
-    # Extract the objects, the object is a pattern of same color
-    objects = find_monochromatic_sprite(input_grid, color_palate=object_colors, background=background)
-    object_list = []
-    for object in objects:
-        x, y, w, h = bounding_box(object, background=background)
-        cropped_object = crop(object, background=background)
-        object_color = np.unique(cropped_object)
-        object_color = object_color[object_color != background][0] 
-        object_list.append({"x": x, "y": y, "w": w, "h": h, "pattern": cropped_object, "color": object_color})
+    # Extract the objects, each of which is a different color.
+    # This means we can split the canvas by color, instead of using connected components.
+    objects = []
+    for color in object_colors:
+        object = np.copy(input_grid)
+        object[input_grid != color] = background
+        objects.append(object)
+    
+    # Define a helper function for checking if two objects are different color/scale but same shape
+    # This has to handle the case where the bigger object is partially outside the grid
+    def same_shape_different_color_different_scale(obj1, obj2):
+        # obj1 is the smaller object
+        if np.sum(obj1 != background) > np.sum(obj2 != background): return False
+
+        mask1 = crop(obj1, background=background) != background
+        mask2 = crop(obj2, background=background) != background
+
+        # Loop through all possible scale factors
+        for scale_factor in range(2, 4):
+            scaled_mask1 = scale_sprite(mask1, scale_factor)
+            # loop over all possible places that we might put mask2, which starts anywhere in the scaled_mask1
+            # note that we are only doing this because there can be objects that fall outside of the canvas
+            # otherwise we would just compare the two masks directly
+            for dx, dy in np.ndindex(scaled_mask1.shape):
+                if np.array_equal(scaled_mask1[dx : dx + mask2.shape[0], dy : dy + mask2.shape[1]], mask2):
+                    return True
+        return False
     
     output_grid_candidates = []
     # Iterate all candidate objects
-    for object in object_list:
-        # Iterate all scale factors
-        for scale_factor in range(2, 4):
-            # Scale the object
-            scaled_pattern = scale_sprite(object["pattern"], scale_factor)
-            
-            # Check if the the other two objects can partially perfect fit scaled object can partially fit 
-            for other_object in object_list:
-                if other_object == object:
-                    continue
-                cur_pattern = other_object["pattern"].copy()
-                cur_w, cur_h = other_object["w"], other_object["h"]
-                # iterate all pixels in the scaled object
-                for x, y in np.argwhere(scaled_pattern):
-                    # Check if the object can partially perfect fit the scaled object
-                    if x + cur_w <= scaled_pattern.shape[0] and y + cur_h <= scaled_pattern.shape[1]:
-                        partial_scaled_grid = scaled_pattern[x : x + cur_w, y : y + cur_h]
-                        
-                        # Change the color of the bigger object to the smaller object for comparison
-                        cur_pattern[cur_pattern == other_object["color"]] = object["color"]
-
-                        # If it fit perfectly, return the output grid
-                        if np.all(partial_scaled_grid == cur_pattern):
-                            # Find the object we want
-                            output_grid_candidates.append(object["pattern"])
-                            break
+    for obj in objects:
+        other_objects = [o for o in objects if o is not obj]
+        if any( same_shape_different_color_different_scale(obj, other_obj) for other_obj in other_objects ):
+            output_grid_candidates.append(obj)
 
     # Check if the generated input grid is valid
-    assert len(output_grid_candidates) == 1, "Should only have one output grid candidate"
+    assert len(output_grid_candidates) == 1, f"Should only have one output grid candidate, have {len(output_grid_candidates)}"
 
-    output_grid = output_grid_candidates[0]
+    output_grid = crop(output_grid_candidates[0], background=background)
+
     return output_grid
 
 def generate_input():
     # get color for background and three objects
-    colors = np.random.choice(Color.NOT_BLACK, 4, replace=False)
-    background, object_small, object_big, object_other = colors
+    background, small_color, big_color, other_color = np.random.choice(Color.NOT_BLACK, 4, replace=False)
 
     # Create background grid
     n, m = np.random.randint(20, 30), np.random.randint(20, 30)
     grid = np.full((n, m), background)
 
-    # Create different two pattern
+    # Create two different sprites
     n1, m1 = np.random.randint(3, 6), np.random.randint(3, 6)
     n2, m2 = np.random.randint(3, 6), np.random.randint(3, 6)
 
-    pattern_small = random_sprite(n=n1, m=m1, color_palette=[object_small], background=background, connectivity=8)
-    pattern_other = random_sprite(n=n2, m=m2, color_palette=[object_other], background=background, connectivity=8)
+    sprite_small = random_sprite(n=n1, m=m1, color_palette=[small_color], background=background, connectivity=8)
+    sprite_other = random_sprite(n=n2, m=m2, color_palette=[other_color], background=background, connectivity=8)
 
-    # Scale the small pattern to the big pattern
+    # Scale the small sprite to the big sprite
     scale_factor = np.random.randint(2, 4)
-    pattern_big = scale_sprite(pattern_small, scale_factor)
-    pattern_big = np.where(pattern_big == object_small, object_big, pattern_big)
+    sprite_big = scale_sprite(sprite_small, scale_factor)
+    # change its color
+    sprite_big[sprite_big == small_color] = big_color
 
-    # Place the big pattern on the grid
-    # The pattern can be partially outside the grid
-    # Ensure the pattern is not too outside the grid
+    # Place the big sprite on the grid
+    # The sprite can be partially outside the grid
+    # Ensure the sprite is not too outside the grid
     padding = 4
     x, y = np.random.randint(padding, n - padding), np.random.randint(padding, m - padding)
-    grid = blit_sprite(grid=grid, sprite=pattern_big, x=x, y=y, background=background)
+    blit_sprite(grid=grid, sprite=sprite_big, x=x, y=y, background=background)
 
     try:
-        # Place the small pattern on the grid
-        x, y = random_free_location_for_sprite(grid=grid, sprite=pattern_small, background=background)
-        grid = blit_sprite(grid=grid, sprite=pattern_small, x=x, y=y, background=background)
+        # Place the small sprite on the grid
+        x, y = random_free_location_for_sprite(grid=grid, sprite=sprite_small, background=background)
+        blit_sprite(grid=grid, sprite=sprite_small, x=x, y=y, background=background)
 
-        # Place the other pattern on the grid
-        x, y = random_free_location_for_sprite(grid=grid, sprite=pattern_other, background=background)
-        grid = blit_sprite(grid=grid, sprite=pattern_other, x=x, y=y, background=background)
+        # Place the other sprite on the grid
+        x, y = random_free_location_for_sprite(grid=grid, sprite=sprite_other, background=background)
+        blit_sprite(grid=grid, sprite=sprite_other, x=x, y=y, background=background)
 
         # Check if the generated grid is valid
         main(grid)
