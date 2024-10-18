25af32ad48b848328bfa78a84e4a3fc6188cf77d
ellisk42@gmail.com <ellisk42@gmail.com>
2024-10-18 17:21:25 -0400

    renamings for 1bfc

--- seeds/1bfc4729.py
+++ seeds/1bfc4729.py
@@ -7,62 +7,64 @@ from typing import *
 # pattern generation
 
 # description:
-# In the input you will see a grid with two pixels.
+# In the input you will see a grid with two colored pixels.
 # To make the output, you should draw a pattern from the two pixels.
-# step 1: draw a horizontal line from the two pixels.
-# step 2: draw a border around the pattern.
-# step 3: fill the top half pattern with the top half pixel color.
-# step 4: fill the bottom half pattern with the bottom half pixel color.
+# step 1: draw a horizontal line outward from the two pixels in their respective colours.
+# step 2: draw a border around the canvas whose top half is colored to match the top pixel and whose bottom half matches the bottom pixel.
 
 
 def main(input_grid):
-    # Output grid is the same size as the input grid
-    output_grid = np.zeros_like(input_grid)
+    # Plan:
+    # 1. Parse the input and determine which pixel is top in which is bottom
+    # 2. Draw horizontal lines
+    # 3. Draw border, colored appropriately
 
-    # Detect the two pixels
-    pixels = find_connected_components(input_grid, monochromatic=True)
-    pixels.sort(key=lambda x: object_position(x)[1])
+    # 1. Input parsing
+    # Detect the two pixels, sorting by Y coordinate so top one is first, bottom one is second
+    background = Color.BLACK
+    pixels = find_connected_components(input_grid, monochromatic=True, background=background)
+    pixels.sort(key=lambda obj: object_position(obj, background=background)[1])
 
-    # Pattern color
-    pattern_color = Color.GRAY
-
-    # STEP 1: Draw a horizontal line from pixels
+    # 2. Draw a horizontal lines outward pixels
+    output_grid = np.full_like(input_grid, background)
     for pixel in pixels:
-        x, y = object_position(pixel)
-        draw_line(output_grid, x=0, y=y, direction=(1, 0), color=pattern_color)
+        x, y = object_position(pixel, background=background)
+        color = object_colors(pixel, background=background)[0]
+        draw_line(output_grid, x=0, y=y, direction=(1, 0), color=color)
     
-    # STEP 2: Draw a border around the pattern
-    draw_line(output_grid, x=0, y=0, direction=(1, 0), color=pattern_color)
-    draw_line(output_grid, x=0, y=0, direction=(0, 1), color=pattern_color)
-    draw_line(output_grid, x=0, y=output_grid.shape[1] - 1, direction=(1, 0), color=pattern_color)
-    draw_line(output_grid, x=output_grid.shape[0] - 1, y=0, direction=(0, 1), color=pattern_color)
-
-    # STEP 3: Fill the top half pattern with the top half pixel color
-    top_pixel = object_colors(pixels[0])[0]
-    output_grid[:, :output_grid.shape[1] // 2][output_grid[:, :output_grid.shape[1] // 2] == pattern_color] = top_pixel
-
-    # STEP 4: Fill the bottom half pattern with the bottom half pixel color
-    bottom_pixel = object_colors(pixels[1])[0]
-    output_grid[:, output_grid.shape[1] // 2:][output_grid[:, output_grid.shape[1] // 2:] == pattern_color] = bottom_pixel
+    # 3. Make a border colored appropriately
+    top_color = object_colors(pixels[0], background=background)[0]
+    bottom_color = object_colors(pixels[1], background=background)[0]
+    draw_line(output_grid, x=0, y=0, direction=(1, 0), color=top_color)
+    draw_line(output_grid, x=0, y=0, direction=(0, 1), color=top_color)
+    draw_line(output_grid, x=0, y=output_grid.shape[1] - 1, direction=(1, 0), color=bottom_color)
+    draw_line(output_grid, x=output_grid.shape[0] - 1, y=0, direction=(0, 1), color=bottom_color)
+    # Everything below the midline is bottom color, everything above is top color
+    # Recolor to enforce this
+    width, height = output_grid.shape
+    top = output_grid[:, :height//2]
+    top[top!=background] = top_color
+    bottom = output_grid[:, height//2:]
+    bottom[bottom!=background] = bottom_color
 
     return output_grid
 
 def generate_input():
-    # Generate the background grid, ensure the grid has an even number of rows
-    n, m = np.random.randint(10, 20, size=2)
-    while (m % 2 == 1):
-        n, m = np.random.randint(10, 20, size=2)
-    grid = np.zeros((n, m), dtype=int)
+    # Generate the grid, ensure the grid has an even height so that we can split it evenly between top and bottom
+    width, height = np.random.randint(10, 20, size=2)
+    while (height % 2 == 1):
+        width, height = np.random.randint(10, 20, size=2)
+    background = Color.BLACK
+    grid = np.full((width, height), background)
 
-    line_distance = 1
-    line_width = 1
     # Randomly choose two colors
     colors = np.random.choice(Color.NOT_BLACK, size=2, replace=False)
 
-    # Randomly place the pixel on the row that will have a line
-    x1, x2 = np.random.choice(range(n), size=2, replace=False)
-    grid[x1, line_distance + line_width] = colors[0]
-    grid[x2, -1 - (line_distance + line_width)] = colors[1]
+    # Randomly place the pixels so that one of them is the in the bottom half and the other in the top half
+    x1, x2 = np.random.choice(range(width), size=2, replace=False)
+    y1, y2 = np.random.randint(2, height//2-1), np.random.randint(height//2 + 1, height-2)
+    grid[x1, y1] = colors[0]
+    grid[x2, y2] = colors[1]
 
     return grid
 
