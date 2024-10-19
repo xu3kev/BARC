2254cc38743c78a2448f0d1560729dbd71e9c265
ellisk42@gmail.com <ellisk42@gmail.com>
2024-10-19 05:09:55 -0400

    generalized 3bdb4ada

--- seeds/3bdb4ada.py
+++ seeds/3bdb4ada.py
@@ -7,30 +7,44 @@ from typing import *
 # punching holes, geometric pattern
 
 # description:
-# In the input you will see a grid with some rectangles. Each rectangle is three pixels tall and is monochromatic.
-# To make the output, you should draw black pixels through the middle have each rectangle with a horizontal period of 2 starting one pixel in from the left.
+# In the input you will see a grid with some rectangles. Each rectangle is three pixels along its short side, and is monochromatic.
+# To make the output, you should draw black pixels along the middle of each rectangle with a period of 2 starting one pixel in.
 # Effectively punching holes in the middle of each rectangle, skipping alternating pixels.
 
 def main(input_grid):
     # Plan:
     # 1. Extract the rectangles from the input grid
-    # 2. Draw black pixels inside each rectangle
+    # 2. Canonicalize the rectangles: ensure that they are horizontal (remember to rotate back otherwise)
+    # 3. Draw black pixels inside each rectangle (horizontally, skipping every other pixel)
+    # 4. Rotate the rectangle back if it was not originally horizontal
 
     # 1. Extract the rectangles from the input grid
     rectangle_objects = find_connected_components(input_grid, monochromatic=True)
 
-    # 2. Draw patterns in rectangles: turn inner pixels to black, alternatingly
     output_grid = np.full_like(input_grid, Color.BLACK)
     for rectangle_object in rectangle_objects:
+        # 2. Canonicalize the rectangle sprite
+        original_x, original_y, width, height = bounding_box(rectangle_object, background=Color.BLACK)
+        # crop to convert object to sprite
         rectangle_sprite = crop(rectangle_object, background=Color.BLACK)
-        w, h = rectangle_sprite.shape
+
+        # Flip it to be horizontal if it isn't already
+        is_horizontal = width > height
+        if not is_horizontal:
+            rectangle_sprite = np.rot90(rectangle_sprite)
+            width, height = height, width
+
+        # 3. Punch holes through the middle of the rectangle
+        # The inner row is y=height//2
+        for x in range(1, width, 2):
+            rectangle_sprite[x, height//2] = Color.BLACK
         
-        # The inner part is h//2
-        for x in range(1, w, 2):
-            rectangle_sprite[x, h//2] = Color.BLACK
+        # 4. Rotate back if it was originally vertical, and then draw it to the output grid
+        if not is_horizontal:
+            rectangle_sprite = np.rot90(rectangle_sprite, k=-1)
         
-        x, y = object_position(rectangle_object)
-        output_grid = blit_sprite(output_grid, rectangle_object, x, y)
+        # draw it back in its original location
+        blit_sprite(output_grid, rectangle_sprite, original_x, original_y)
 
     return output_grid
 
@@ -40,14 +54,18 @@ def generate_input():
     grid = np.zeros((width, height), dtype=int)
 
     # Randomly choose the number of rectangles on the grid
-    rectangle_number = np.random.randint(1, 4)
+    num_rectangles = np.random.randint(1, 4)
     # Randomly choose colors for the rectangles
-    colors = np.random.choice(Color.NOT_BLACK, rectangle_number, replace=False)
+    colors = np.random.choice(Color.NOT_BLACK, num_rectangles, replace=False)
 
     for color in colors:
         # Randomly choose the width of the rectangle, should not be too short and should be odd
         w, h = np.random.randint(2, 6) * 2 + 1, 3
         rectangle_sprite = np.full((w, h), color)
+
+        # randomly flipped to create different orientations
+        if np.random.rand() < 0.5:
+            rectangle_sprite = np.rot90(rectangle_sprite)
 
         # Randomly choose the position of the rectangle
         try:
