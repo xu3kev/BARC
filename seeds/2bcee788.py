a17075ab012766c8b33bc67a45b7f58551428c69
ellisk42@gmail.com <ellisk42@gmail.com>
2024-10-19 07:55:58 -0400

    2bcee788 cleanup

--- seeds/2bcee788.py
+++ seeds/2bcee788.py
@@ -11,13 +11,13 @@ from typing import *
 # To make the output, mirror the object to cover the red pixels. Then change the background to green.
  
 def main(input_grid):
-    # unfinished
     # Plan:
     # 1. Find the main object and the red pixels
     # 2. Calculate the axis over which to mirror depending on which side the red pixels are on
     # 3. Do the mirroring
     # 4. Change background to green
     
+    # 1. Input parsing
     objects = find_connected_components(input_grid, connectivity=8, background=Color.BLACK, monochromatic=True)
     assert len(objects) == 2, "There should be exactly two objects"
 
@@ -26,20 +26,24 @@ def main(input_grid):
     # Find the red pixels
     red_pixels = next(obj for obj in objects if Color.RED in object_colors(obj, background=Color.BLACK))
 
+    # 2. Axis calculation
+
     # Figure out what side of the object the red pixels are on
+    x1, y1 = object_position(main_object, anchor="upper left")
+    x2, y2 = object_position(main_object, anchor="lower right")
     # on the right?
     if collision(object1=translate(main_object, x=1, y=0), object2=red_pixels):
         # the +/- 0.5 is to clobber the red pixels, otherwise we'd reflect over them and leave them be, which would also be a reasonable thing to do
-        symmetry = MirrorSymmetry(mirror_x=object_position(main_object, anchor="right center")[0]+0.5, mirror_y=None)
+        symmetry = MirrorSymmetry(mirror_x=x2+0.5, mirror_y=None)
     # on the left?
     elif collision(object1=translate(main_object, x=-1, y=0), object2=red_pixels):
-        symmetry = MirrorSymmetry(mirror_x=object_position(main_object, anchor="left center")[0]-0.5, mirror_y=None)
+        symmetry = MirrorSymmetry(mirror_x=x1-0.5, mirror_y=None)
     # on the top?
     elif collision(object1=translate(main_object, x=0, y=-1), object2=red_pixels):
-        symmetry = MirrorSymmetry(mirror_x=None, mirror_y=object_position(main_object, anchor="top center")[1]-0.5)
+        symmetry = MirrorSymmetry(mirror_x=None, mirror_y=y1-0.5)
     # on the bottom?
     elif collision(object1=translate(main_object, x=0, y=1), object2=red_pixels):
-        symmetry = MirrorSymmetry(mirror_x=None, mirror_y=object_position(main_object, anchor="bottom center")[1]+0.5)
+        symmetry = MirrorSymmetry(mirror_x=None, mirror_y=y2+0.5)
     else:
         assert False, "Red pixels are not on any side of the main object"
     
@@ -48,7 +52,8 @@ def main(input_grid):
     blit_object(output_grid, main_object)
     for x, y in np.argwhere(main_object != Color.BLACK):
         for x2, y2 in orbit(output_grid, x, y, symmetries=[symmetry]):
-            output_grid[x2, y2] = main_object[x, y]
+            if 0 <= x2 < output_grid.shape[0] and 0 <= y2 < output_grid.shape[1]:
+                output_grid[x2, y2] = main_object[x, y]
     
     # Change the background to green
     output_grid[output_grid == Color.BLACK] = Color.GREEN
